1457d17e96524414b6781aa5ee2ac8aa2639f143
tbs283 <2017firstisorion@gmail.com>
2021-04-30 11:01:23 +0000

    “[Finish]3-8”

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,11 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-  end
-
-  def new
-    @books = Book.all
-    @book = Book.new
   end
   
   def create
@@ -26,6 +22,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
